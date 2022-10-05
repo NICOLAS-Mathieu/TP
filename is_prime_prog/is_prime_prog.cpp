963734f65b0dfced9f7d7d577803b666fe6ad4b6
NICOLAS-Mathieu <104777200+NICOLAS-Mathieu@users.noreply.github.com>
2022-10-05 10:23:26 +0200

    Add files via upload

---
+++ is_prime_prog/is_prime_prog.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+bool isPrime(int nb)
+{
+	if (nb <= 1)
+	{
+		return false;
+	}
+	if (nb == 2)
+	{
+		return true;
+	}
+	for (int i = 2; i < ceil(sqrt(nb)) + 1; i++)
+	{
+		if (nb % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+bool main()
+{
+	int nb;
+	while (cin >> nb)
+	{
+		if (isPrime(nb))
+		{
+			cout << nb << "is a prime : True \n";
+		}
+		else
+		{
+			cout << nb << "is a prime : False \n";
+		}
+	}
+}
